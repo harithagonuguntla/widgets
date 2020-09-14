f2088ad761e74ffd72777cae3444dd9cdfef69ce
Haritha Gonuguntla <Haritha@Harithas-MacBook-Pro.local>
2020-09-14 17:44:40 -0500

    added search.js

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front end Javascript framework'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is favourite JS library among engineers'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by creating components'
+    }
+]
+
+export default () => {
+    return (
+        <div>
+            {/* <Accordion items={items}/> */}
+            <Search />
+        </div>
+        )
+}
